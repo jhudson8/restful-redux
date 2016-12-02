e227c46e35dc0a287d121ff470d319621f3e7e9c
Joe Hudson <joehud@gmail.com>
2016-12-02 11:39:04 -0500

    update examples

--- examples/01-github-profile-viewer/local-copy.sh
+++ examples/01-github-profile-viewer/local-copy.sh
@@ -3,4 +3,4 @@
 rm -rf node_modules/react-redux-model/lib/*
 cd ../..
 npm run build
-cp -rf lib/* examples/01-github-profile-viewer/node_modules/react-redux-model/lib
+cp -rf lib examples/01-github-profile-viewer/node_modules/react-redux-model
--- examples/05-normalizr-github-paged-project-search-and-viewer/local-copy.sh
+++ examples/05-normalizr-github-paged-project-search-and-viewer/local-copy.sh
@@ -3,4 +3,4 @@
 rm -rf node_modules/react-redux-model/lib/*
 cd ../..
 npm run build
-cp -rf lib/* examples/05-normalizr-github-paged-profile-search-and-viewer/node_modules/react-redux-model/lib
+cp -rf lib examples/05-normalizr-github-paged-project-search-and-viewer/node_modules/react-redux-model
